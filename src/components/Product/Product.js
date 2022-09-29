f1e3da5dab42a1cb2f118dbac5b6dc79ae27fc2e
toukircse <toukirahmed610@gmail.com>
2022-09-29 09:10:42 +0600

    create cart dynamically

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './Product.css'
 
 const Product = ({cart}) => {
+    const {title,time,url,ratings} = cart;
     return (
-        <div>
-            <h2>name: {cart.title}</h2>
+        <div className='cart'>
+            <img src={url} alt="" />
+            <div className='cart-info'>
+                <h2>Skill: {title}</h2>
+                <p>Duration: {time} sec</p>
+                <p>ratings: {ratings}</p>
+            </div>
+            <button>Add to Cart</button>
         </div>
     );
 };
