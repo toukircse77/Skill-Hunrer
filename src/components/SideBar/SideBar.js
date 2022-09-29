d3b2d8fc70e492217879f3a519b63799a0784509
toukircse <toukirahmed610@gmail.com>
2022-09-29 11:24:14 +0600

    creat practise Details

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Break from '../Break/Break';
 import Profile from '../Profile/Profile';
+import './SideBar.css'
 
 const SideBar = () => {
     return (
@@ -8,6 +9,13 @@ const SideBar = () => {
             <Profile></Profile>
             <h3>Add A Break</h3>
             <Break></Break>
+            <h3>Practise Details</h3>
+            <div className='exercise'>
+                <p>Practise time:</p>
+            </div>
+            <div className='exercise'>
+                <p>Break time:</p>
+            </div>
         </div>
     );
 };
